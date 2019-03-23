2c487d412799356bd754a034a014bf7e0139ea69
Mark Snelling <mark@bakedbeans.com>
2019-03-23 14:32:02 +0000

    Updated vault password script

--- get_vault_pass.sh
+++ get_vault_pass.sh
@@ -3,13 +3,13 @@ set -e
 
 # MacOS
 # to store (-l is the label, mb will be different later):
-# $ security add-generic-password -a all -D "Ansible Vault" -s "ansible_vault" -l "pass-kind" -w 'pass-here!'
+# $ security add-generic-password -a ${USER} -D "Ansible Vault" -s "ansible_vault" -l "vault-id" -w 'pass-here!'
 # ansible allows to prompt for data (to stdout) and have different passwords here
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     vault_pass=$(cat .vault_id);;
-    Darwin*)    vault_pass=$(security find-generic-password -s "ansible_vault" -l "acme-certs" -w);;
+    Darwin*)    vault_pass=$(security find-generic-password -s "ansible_vault" -l "vault-id" -w);;
     *)          raise error "Unsupported OS '${unameOut}'"
 esac
 echo ${vault_pass}
